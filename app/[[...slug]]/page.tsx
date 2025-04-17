1f41a4031324f7c03717cf1d38f15d00cd926c04
Harshal0902 <harshalraikwar07@gmail.com>
2025-04-17 14:36:03 +0530

    📝 Add Referral page

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -4,15 +4,8 @@ import { DocsPage, DocsBody, DocsDescription, DocsTitle } from 'fumadocs-ui/page
 import { notFound } from 'next/navigation'
 import { Step, Steps } from 'fumadocs-ui/components/steps'
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom'
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/app/_components/table"
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/app/_components/table'
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 
 export default async function Page({ params }: { params: { slug?: string[] } }) {
@@ -45,6 +38,8 @@ export default async function Page({ params }: { params: { slug?: string[] } }) 
           TableHead,
           TableHeader,
           TableRow,
+          Accordion,
+          Accordions,
           img: (props) => <ImageZoom {...(props as any)} />,
         }} />
       </DocsBody>
